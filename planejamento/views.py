4382739d86d1c745998ba66c8d859f145b601426
Pedro Bressane <137115418+PedroBressane@users.noreply.github.com>
2023-07-16 22:17:38 -0300

    Update views.py

--- planejamento/views.py
+++ planejamento/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
 from perfil.models import Categoria
 from django.http import JsonResponse
+from django.contrib import messages
+from django.contrib.messages import constants
 from django.views.decorators.csrf import csrf_exempt
 import json
 
-# Create your views here.
 
 def definir_planejamento(request):
     categorias = Categoria.objects.all()
@@ -16,8 +17,9 @@ def update_valor_categoria(request, id):
     categoria = Categoria.objects.get(id=id)
     categoria.valor_planejamento = novo_valor
     categoria.save()
-
+    messages.add_message(request,constants.SUCCESS, 'Categoria atualizada com sucesso')
     return JsonResponse({'status': 'Sucesso'})
+
 
 def ver_planejamento(request):
     #realizar barra com total de gasto
